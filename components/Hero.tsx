69b026e03b959b744f802e360448e6ffcad575f9
Richard <richard.atodo@yahoo.com>
2024-07-26 20:59:21 +0100

    About section done with BentoGrid, Globe etc

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,11 +31,11 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Applications with AWS
+            Dynamic Cloud-Native Application
           </p>
 
           <TextGenerateEffect
-            words="Designing & Implementing Effective & Scalable Solutions on the Cloud"
+            words="Implementing Effective Human Centric Solutions on the Cloud"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
